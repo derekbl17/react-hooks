c65bed639008613049b6f9571ba5744431649b35
derekbl17 <derekc1fill@gmail.com>
2025-03-17 13:16:00 +0200

    z

--- src/components/Vaikas.jsx
+++ src/components/Vaikas.jsx
@@ -1,11 +1,14 @@
-import {useContext} from 'react'
-import { Context } from '../assets/context'
+import { useContext } from "react";
+import { Context } from "../assets/context";
 
 export default function Vaikas() {
-    const {text,textColor,textFont,backgroundColor}=useContext(Context)
+  const { text, textColor, textFont, backgroundColor, numb } =
+    useContext(Context);
   return (
-    <div style={{background:backgroundColor}}>
-        <h3 style={{color:textColor, fontFamily:textFont}}>{text}</h3>
+    <div style={{ background: backgroundColor }}>
+      <h3 style={{ color: textColor, fontFamily: textFont }}>
+        {text} {numb}
+      </h3>
     </div>
-  )
+  );
 }
